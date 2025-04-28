e371647ec006c897aba5a441c7a652e1bf22eff5
Joakim Ekström <joakim.ekstrom@gmail.com>
2025-04-28 22:22:07 +0200

    refactor: Move includes to top and reorder alphabetically

--- custom_behavior/behavior_cycle_string.c
+++ custom_behavior/behavior_cycle_string.c
@@ -7,24 +7,27 @@
 
 #define DT_DRV_COMPAT chord_cycle_behavior_cycle_string
 
+#include <drivers/behavior.h>
 #include <zephyr/device.h>
-#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
 #include <zmk/behavior.h>
-#include <zmk/behavior_queue.h> // For zmk_behavior_queue_add
+
+/* <zmk/keymap.h> */
+/* #include <zmk/behavior_queue.h> // For zmk_behavior_queue_add */
 /* #include <zmk/event_manager.h> */
-/* #include <zmk/events/keycode_state_changed.h> // For keycode events used by
- * macros */
-/* #include <zmk/events/modifiers_state_changed.h> // For modifier events used
+/* #include <zmk/events/keycode_state_changed.h> // For keycode events used
  * by macros */
+/* #include <zmk/events/modifiers_state_changed.h> // For modifier events
+ * used by macros */
 /* #include <zmk/events/position_state_changed.h> */
 /* #include <zmk/hid.h> // For zmk_hid_get_keycode_t, HID usage IDs */
 /* #include <zmk/keymap.h> */
 /**/
 /* // Include macro helper header */
 /* #include <zmk/behavior/macro.h> */
-
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 // Define the strings to cycle through
 static const char *cycle_strings[] = {"work", "working"};
